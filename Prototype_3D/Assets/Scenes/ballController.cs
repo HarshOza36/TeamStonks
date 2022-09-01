28b2a4e86a5e995e33da1a657eb9f01586ffc545
Ke <kewei423@gmail.com>
2022-08-31 21:54:18 -0700

    get a ball jump up with space bar

---
+++ Prototype_3D/Assets/Scenes/ballController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballController : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")){
+            rb.AddForce (new Vector3 (0f, 500f, 0f));
+        }
+    }
+}
