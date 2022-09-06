29ce0ba25a9e739c4230d1958884512cec7d0d99
Adishri_Gupta <37748234+Adishri-Gupta@users.noreply.github.com>
2022-09-05 22:35:05 -0700

    Space Bar jumping mechanism added

--- prototype_2d/Assets/Scripts/boxMovement.cs
+++ prototype_2d/Assets/Scripts/boxMovement.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class boxMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    public float jump;
+
     private Rigidbody2D body;
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
     }
     private void Update() {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical")*speed);
+        // body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical")*speed);
+        if (Input.GetButtonDown("Jump")){
+            body.AddForce(new Vector2(0, jump));
+        }
 
     }
 }
