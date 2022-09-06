a38db7ff0af2e91a001aacd6de3be608526d2bc0
Shail <anonshail@gmail.com>
2022-09-05 23:17:53 -0700

    added debounce

--- prototype_2d/Assets/Scripts/boxMovement.cs
+++ prototype_2d/Assets/Scripts/boxMovement.cs
@@ -4,7 +4,7 @@ public class boxMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     public float jump;
-
+    private float canJump = 0f;
     private Rigidbody2D body;
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
@@ -13,8 +13,9 @@ public class boxMovement : MonoBehaviour
     }
     private void Update() {
         // body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical")*speed);
-        if (Input.GetButtonDown("Jump")){
+        if (Input.GetButtonDown("Jump") && Time.time > canJump){
             body.AddForce(new Vector2(0, jump));
+            canJump = Time.time + 0.13f;
         }
 
     }
