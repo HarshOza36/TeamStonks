3677af1e36ba20fadf95f2b2c97c937cb4d2a1b3
HarshOza36 <harshoza3636@gmail.com>
2022-09-21 16:26:35 -0700

    Change Analytics to Google Docs

--- Prototype_3D/Assets/Scripts/ballController.cs
+++ Prototype_3D/Assets/Scripts/ballController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Networking;
 using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 
@@ -14,11 +15,26 @@ public class ballController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+
+        var val = 1;
+        StartCoroutine(Post(val.ToString()));
+        
+
+    
         audioData = GetComponent<AudioSource>();
         //restart = GetComponent<restart>
         rb = GetComponent<Rigidbody>();
         timeRemaining = GetComponent<timer>();
         //Debug.Log(timeRemaining.timeRemaining);
+    }
+    IEnumerator Post(string s1){
+        string URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSc4UIvbJ8V5byuWfF3iPFXURGq4PtwJlSxTwjOYyHQJujeodQ/formResponse";
+        WWWForm form = new WWWForm();
+        form.AddField("entry.488519728", s1);
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
     }
 
     // Update is called once per frame
