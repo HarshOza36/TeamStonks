24944c3f114cadfc1ef41b775c1c04262ddbba21
Adishri-Gupta <adishri.gupta@gmail.com>
2022-10-05 14:55:59 -0700

    camera position fixed

--- Prototype_3D/Assets/Scripts/playerCamera.cs
+++ Prototype_3D/Assets/Scripts/playerCamera.cs
@@ -10,7 +10,7 @@ public class playerCamera : MonoBehaviour
 
     void Start()
     {
-        offset = transform.position - ball.position;
+        offset = transform.position - 4*ball.position;
     }
 
     // Update is called once per frame
