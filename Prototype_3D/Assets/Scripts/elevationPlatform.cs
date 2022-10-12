4d1f6d981ca10905bda828e022d4c6c3bf511e6d
Ke <kewei423@gmail.com>
2022-10-12 13:29:04 -0700

    [fix] elevation platform centered with the pip

--- Prototype_3D/Assets/Scripts/elevationPlatform.cs
+++ Prototype_3D/Assets/Scripts/elevationPlatform.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class elevationPlatform : MonoBehaviour
 {
-    private float ogLocation;
+    private float ogLocation_y;
+    private float ogLocation_x;
+    //public GameObject center_pipe_A;
+    //public GameObject center_pipe_B;
     public float speed;
     public float elevatingRange;
-    public GameObject player_ball;
+    //public GameObject player_ball;
     // Start is called before the first frame update
     void Start()
     {
-        ogLocation = transform.position.y;
-        //Debug.Log("og location: " + ogLocation);
+        ogLocation_y = transform.position.y;
+        //ogLocation_x = transform.position.x;
+        //center_pipe.gameObject.transform.position.x = 
+        //Debug.Log("platform location: " + transform.position);
+        //Debug.Log("pip location: " + center_pipe_A.transform.position);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float y = Mathf.PingPong(Time.time * speed, elevatingRange) +ogLocation;
+        float y = Mathf.PingPong(Time.time * speed, elevatingRange) +ogLocation_y;
         //Debug.Log("now og location: " + ogLocation);
         //Debug.Log("ping pong y: " + y);
-        transform.position = new Vector3(0, y, 0);
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        Debug.Log("new posiiton: " + transform.position);
+
     }
 
 }
