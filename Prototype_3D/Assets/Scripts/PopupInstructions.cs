20d6b3b64e0831ea82aa73e97f581a87a5f81349
Minkang Zhang <minkangz@usc.edu>
2022-11-08 07:29:59 -0800

    Fixed the tutorial pop-up and add two new tutorials

--- Prototype_3D/Assets/Scripts/PopupInstructions.cs
+++ Prototype_3D/Assets/Scripts/PopupInstructions.cs
@@ -7,19 +7,38 @@ public class PopupInstructions : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject uiObject;
+    public bool pause;
     void Start()
     {
         uiObject.SetActive(false);
+    }
+
+    void Update() 
+    { 
+	if (Time.timeScale == 0 && Input.anyKey) { 
+		Time.timeScale = 1; 
+                Destroy(uiObject);
+        	Destroy(gameObject);
+	} 
     }
 
     // Update is called once per frame
     void OnTriggerEnter (Collider player) {
         
         if (player.gameObject.tag == "Player")
-        {
-            uiObject.SetActive(true);
-            StartCoroutine("WaitForSec");
+        {   
+            if(pause != true){	   
+            	uiObject.SetActive(true);
+            	StartCoroutine("WaitForSec");
+            }else{
+		PauseGame();	
+	    }
         }
+    }
+
+    public void PauseGame(){
+	uiObject.SetActive(true);
+	Time.timeScale = 0;	
     }
 
     IEnumerator WaitForSec(){
