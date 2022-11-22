1072d49eb2c16ee94a100b57ef2156fd87d5e1d2
yohan9655 <yohanvarghese9655@gmail.com>
2022-11-21 20:06:53 -0800

    Added transition for tutorial menu

--- Prototype_3D/Assets/Scripts/tutorialMenu.cs
+++ Prototype_3D/Assets/Scripts/tutorialMenu.cs
@@ -6,7 +6,27 @@ using UnityEngine.Networking;
 
 public class tutorialMenu : MonoBehaviour
 {
- public void LoadScene(string sceneName){
-        SceneManager.LoadScene(sceneName);
+    public RectTransform fader;
+    private bool transition;
+    private void Start() {
+        fader = GameObject.FindGameObjectWithTag("transition").GetComponent<RectTransform>();
+        fader.gameObject.SetActive(true);
+        LeanTween.alpha(fader, 1, 0);
+        transition = true;
+    }
+    private void Update() {
+        if (transition) {
+            transition = false;
+            LeanTween.alpha(fader, 0, 1f).setOnComplete(() =>{
+            fader.gameObject.SetActive(false);
+        });
+        }
+    }
+    public void LoadScene(string sceneName){
+        fader.gameObject.SetActive(true);
+        LeanTween.alpha(fader, 0, 0);
+        LeanTween.alpha(fader, 1, 0.5f).setOnComplete(() =>{
+            SceneManager.LoadScene(sceneName);
+        });
     }
 }
